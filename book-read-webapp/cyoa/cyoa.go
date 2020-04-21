700a21e10be48b81ef07acfff381969626ee7de2
Sunitha Nair <sunitha.p@iqzsystems.com>
2020-04-21 23:55:25 +0530

    parsing paths

--- book-read-webapp/cyoa/cyoa.go
+++ book-read-webapp/cyoa/cyoa.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"io"
 	"net/http"
+	"strings"
 	"text/template"
 )
 
@@ -12,17 +13,20 @@ var defaultHTMLHandler = `
 <html>
   <head>
     <meta charset="utf-8">
-    <title>Choose your own adventure</title>
+	<title>Choose your own adventure</title>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.8.2/css/bulma.min.css">
     <meta name="description" content="This is an example of a meta description.">
   </head>
   <body>
-		<h1>{{.Title}}</h1>
+		<h1 class="subtitle is-1 has-text-centered">{{.Title}}</h1>
 		{{range .Paragraph}}
-		<p>{{.}}</p>
+		<p class="container">{{.}}</p>
 		{{end}}
-		<div style="display:flex;justify-content: center">
+		<div class="field is-grouped is-grouped-centered is-grouped-multiline">
 			{{range .Options}}
-			<a href="/{{.Arc}}">{{.Text}}</a>
+			<div class="control">
+			<a class="button is-link" href="/{{.Arc}}">{{.Text}}</a>
+			</div>
 			{{end}}
 		</div>
   </body>
@@ -52,7 +56,7 @@ type Chapter struct {
 // ChapterOption ..
 type ChapterOption struct {
 	Text string `json:"text"`
-	Arc  string `json:"arc_name"`
+	Arc  string `json:"arc"`
 }
 
 //NewHandler ..
@@ -65,9 +69,15 @@ type handler struct {
 }
 
 func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	path := strings.TrimSpace(r.URL.Path)
+	if path == "" || path == "/" {
+		path = "/intro"
+	}
+	path = path[1:]
+
 	tpl := template.Must(template.New("").Parse(defaultHTMLHandler))
-	err := tpl.Execute(w, h.s["intro"])
+	err := tpl.Execute(w, h.s[path])
 	if err != nil {
-		panic(err)
+		http.Error(w, "Not Found", http.StatusNotFound)
 	}
 }
